dd8020318f92e4c884eae798e3eb665cb943cc25
jorgelmp <jlzphendrix@gmail.com>
2020-12-31 12:59:06 -0600

    Con el código comentado.

--- src/main/S19generator.java
+++ src/main/S19generator.java
@@ -13,10 +13,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * Clase cuya instancias se utilizan para generar archivos con el código objeto 
+ * en formato Motorola en extensión S19  y en HTML con opcodes y operandos en co
+ * lores azul y rojo respectivamente
  * @author jorge
  */
 public class S19generator {
+    
     private int dirActual = 0;
     private int extra = 0;
     private int lengActual= 0;
@@ -41,9 +44,14 @@ public class S19generator {
     private String codigoHtml="";
     private String iniciHtml = "";
     private String finalHtml= "";
-    
-    //class Html
-    
+
+    /**
+     * Construye un generador s19
+     * @param filename nombre del archivo
+     * @param continuar booleano para saber si se debe continuar con la construc
+     * cción del s19, depende de los errores en el código.
+     * @throws IOException 
+     */    
     public S19generator(String filename, boolean continuar) throws IOException{
         this.generar = continuar;
         if(generar){
@@ -59,10 +67,16 @@ public class S19generator {
         aImprimir = primera;
     }
     
+    /**
+     * Agrega una nueva dirección al código objeto
+     * @param dirOrg la dirección nueva
+     * @param continuar booleano para saber si aún no hay errores en código
+     * @throws IOException 
+     */
     public void buildLine(String dirOrg, boolean continuar) throws IOException{
         this.generar = continuar;
         if(!generar)
-            return;
+            return; //Si sí hay errores no hace nada
         escribe();
         dirActual = Integer.parseInt(dirOrg,16);
         lengActual = 0;
@@ -70,10 +84,19 @@ public class S19generator {
         //sumaActual += toInt(dirActual);
     }
     
+    /**
+     * Agrega una nueva instrucción al código objeto, caracterizada por un par 
+     * opcode, operando (puede que el operando sea nulo). Si se le indica que la
+     * línea actual se ha llenado, la imprime e inicia la línea siguiente.
+     * @param opcode opcode la instrucción en hexadecimal
+     * @param operando operando de la instrucción en hexadecimal
+     * @param continuar booleano para saber si aún no hay errores en código
+     * @throws IOException 
+     */
     public void buildLine(String opcode, String operando, boolean continuar) throws IOException{
         this.generar = continuar;
-        if(!generar)
-            return;
+        if(!generar) 
+            return; //Si sí hay errores no hace nada
         
         if(!org){
             escribe();
@@ -101,6 +124,12 @@ public class S19generator {
         }
     }
     
+    /**
+     * Se llama indicar que se ha terminado de construir el código objeto. Es-
+     * cribe la línea actual y la última línea en el S19 y en el HTML y cierra 
+     * los escritores.
+     * @throws IOException 
+     */
     public void end() throws IOException{
         if(!generar){
             //file.deleteOnExit();
@@ -125,6 +154,15 @@ public class S19generator {
         }
     }
     
+    /**
+     * Agrega un nuevo valor hexadecimal al código objeto, analiza si es posible 
+     * acomodarlo en la línea actual, si no, actualiza el atributo con la canti
+     * dad que sobra e indica que es necesario imprimir la línea actual, pues 
+     * está llena.
+     * @param toAdd lo que se agregará
+     * @param opcode booleano para saber si lo que se agregará es opcode u operando
+     * @return true, si y sólo si, es necesario imprimir la línea actual
+     */
     private boolean addC(String toAdd, boolean opcode){
         int extra = sePasaPor(toAdd);
         String color = (opcode)? "blue" : "red";
@@ -156,15 +194,28 @@ public class S19generator {
         }
     }
     
+    /**
+     * Construye la línea a imprimir en el s19 con el registro S1, la longitud en
+     * bytes de la información a imprimir, la dirección desde la cual parte el 
+     * código, el código mismo (los datos) y el checksum
+     */
     private  void construyeLinea(){
         aImprimir = defaultField + toHex(lengActual + 3,2)+ toHex(dirActual,4) + codigo;
         aImprimir += checkSum();
     }
     
+    /**
+     * Construye la línea a imprimir en el HTML de la misma manera que en el s19
+     * y agrega el tipo de letra que debe usarse.
+     */
     private void construyeHtml(){
         aHtml = "<p style = \"font-family:Courier New,Monaco,monospace;font-size:16px;\">"+defaultField + toHex(lengActual+3,2)+toHex(dirActual,4) + codigoHtml + checkSum + "</p>";
     }
     
+    /**
+     * Inicializa el HTML 
+     * @throws IOException 
+     */
     private void initHtml() throws IOException{
         String inicial = 
                 "<!DOCTYPE html>"
@@ -182,26 +233,53 @@ public class S19generator {
         bhescritor.flush();
     }
     
+    /**
+     * Calcula por cuanto se pasa un valor a añadir en el código objeto.
+     * @param aAnadir lo que se quiere añadir
+     * @return un entero positivo si sí se pasa, un entero negativo no se pasa o
+     * cero no queda espacio después de acomodarlo
+     */
     private int sePasaPor(String aAnadir){
         int nuevo = aAnadir.length()/2;
         return lengActual+nuevo-max;        
     }
     
+    /**
+     * Convierte a entero decimal una cadena que representa un valor hexadecimal
+     * @param cadenaHex la cadena en decimal
+     * @return el valor entero
+     */
     private int toInt(String cadenaHex){
-
         return Integer.parseInt(cadenaHex, 16);
     }
     
+    /**
+     * Convierte a hexadecimal un entero a un formato de len digitos
+     * @param a el entero a convertir
+     * @param len los dígitos del formato
+     * @return una cadena en hexadecimal
+     */
     private String toHex(int a, int len){
         return  String.format("%0"+len+"X", a);
     }
     
+    /**
+     * Calcula el checksum de la línea a imprimir. El checksum es el complemento
+     * a uno del byte menos significativo de la suma de los valores que siguen a
+     * la bandera de registro SX.
+     * @return el checksum de la línea actual
+     */
     private String checkSum(){
         String[] pairs = aImprimir.substring(2).split("(?<=\\G.{2})");
         String sum = sumAt8B(pairs);
         return checkSum = String.format("%02X", 255 - Integer.parseInt(sum, 16));
     }
     
+    /**
+     * Suma los bytes que siguen a SX en la línea actual.
+     * @param pairs
+     * @return 
+     */
     private String sumAt8B(String[] pairs){
         int sum = 0;
         for(String hex: pairs){
@@ -210,6 +288,12 @@ public class S19generator {
         String hex = Integer.toHexString(sum);
         return hex.substring(hex.length()-2);
     }
+    
+    /**
+     * Escribe una línea nueva en los archivos S19 y HTML y actualiza los valores
+     * de la línea actual a los de una línea vacía.
+     * @throws IOException 
+     */
     private void escribe() throws IOException{
         if(!generar)
             return;
